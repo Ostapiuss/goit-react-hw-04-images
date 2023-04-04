eaaa15d823c3b985478a266a1ecbf75611d8f5b5
ostap <ostapokolovych@gmail.com>
2023-04-04 12:28:39 +0300

    [FE] fix

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,6 +1,23 @@
 import './style.scss'
+import {useEffect} from "react";
 
 const Modal = ({ imageSrc, imageTag, isActive, setIsActive }) => {
+
+  const onEscClose = (keyEvent) => {
+    if (keyEvent.key === 'Escape') {
+      setIsActive(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', onEscClose);
+
+    return () => {
+      window.removeEventListener('keydown', onEscClose)
+    }
+  // eslint-disable-next-line
+  }, []);
+
   return (
     <>
       {
